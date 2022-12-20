3193c3fb20ecdfdd61d259dffa11c0e1cf672344
OsmarG <osmar_gimenez@outlook.com>
2022-12-20 16:46:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*funcion para cambiar el texto de los botones de On a Off*/
+
+function turnOff(element) {
+    // innerText es el que permite realizar el cambio de texto.
+    element.innerText = "Off";
+}
+
+/*funcion para remover elementos al hacer click sobre ellos*/
+function hide(element) {
+    //remove es el que permite esconder los elementos
+    element.remove();
+}
